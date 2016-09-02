4b56124fb9e2f60f97eb5691d88077d1aceac8c6
gazzal <u1365222@unimail.hud.ac.uk>
2016-09-02 03:51:55 +0000

    Display message if the show has ended

--- getNextEpisodeInfo.sh
+++ getNextEpisodeInfo.sh
@@ -8,18 +8,22 @@ do
   if [ ${#name} = 0 ]; then echo "No match found for '$show'"
   else
     echo "Show: $name"
-    nextepisode=$(echo $showInfo | sed 's/.*"nextepisode"://')
-    if [ "$nextepisode" != "$showInfo" ]; then
-      airdate=$(echo $showInfo | sed 's/.*"airdate":"\([^"]*\)".*/\1/')
-      airtime=$(echo $showInfo | sed 's/.*"airtime":"\([^"]*\)".*/\1/')
-      if [ ${#airdate} = 0 ]; then
-        if [ ${#airtime} = 0 ]; then echo "No broadcast information is known"; else echo "Next episode airs at $airtime (date not known)"; fi
-      else
-        echo -ne "Next episode: $airdate"
-        if [ ${#airtime} != 0 ]; then echo " @ $airtime"; else echo ""; fi
-      fi
+    status=$(echo $showInfo | sed 's/.*"status":"\([^"]*\)".*/\1/')
+    if [ "$status" == "Ended" ]; then echo "$name has ended; there are no more episodes"
     else
-      echo "Next episode has not been added for this show"
+      nextepisode=$(echo $showInfo | sed 's/.*"nextepisode"://')
+      if [ "$nextepisode" != "$showInfo" ]; then
+        airdate=$(echo $showInfo | sed 's/.*"airdate":"\([^"]*\)".*/\1/')
+        airtime=$(echo $showInfo | sed 's/.*"airtime":"\([^"]*\)".*/\1/')
+        if [ ${#airdate} = 0 ]; then
+          if [ ${#airtime} != 0 ]; then echo "No broadcast information is known"; else echo "Next episode airs at $airtime (date not known)"; fi
+        else
+          echo -ne "Next episode: $airdate"
+          if [ ${#airtime} != 0 ]; then echo " @ $airtime"; else echo ""; fi
+        fi
+      else
+        echo "Next episode has not been added for this show"
+      fi
     fi
   fi
 done
